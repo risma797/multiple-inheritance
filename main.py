8f1c78cec19d46c4c018f6c2660af2402b2078dd
Risma Nurhayati <80274102+risma797@users.noreply.github.com>
2021-12-26 17:01:42 +0000

    ubah multiple inheritence

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-#Kelas utama
 class Laptop:
-  SSD_sekarang = 256
+  def spesifikasi(self):
+    print('Dell, 8 GB, 256 SSd, Intel Core 7 ')
 
-#fungsi construktor
-  def __init__ (self, nama_merk, kapasitas_RAM, CPU_processor):
-    self.nama_merk = nama_merk
-    self.kapasitas_RAM = kapasitas_RAM
-    self.CPU_processor = CPU_processor
-    print(f'Laptop {self.nama_merk},{self.kapasitas_RAM}, {self.CPU_processor} dioperasikan')
+class Fungsi:
+  def kegunaan(self):
+    print('Help get the job done')
 
-    def menambahSSD(self, menambahSSD):
-      self.SSD_sekarang = self.SSD_sekarang + menambahSSD
-      print('SSD_sekarang ', self.SSD_sekarang)
-   
-#destructor
-  def __del__ (self):
-    print(f'Laptop {self.nama_merk},{self.kapasitas_RAM}, {self.CPU_processor} dimatikan')
-  
+class My_Laptop(Laptop, Fungsi):
+  pass
 
-#membuat objek
-laptop_kuliah = Laptop('Dell','8 GB','Intel core I7')
-laptop_kerja = Laptop('Macbook','16 GB','Mac OS' )
-
-print(' ')
-print('=================================================')
-print(' ')
-
-del laptop_kuliah
-del laptop_kerja
-
-print(' ')
-print('=================================================')
-print(' ')
-
-print('SSD Laptop Sekarang :', Laptop.SSD_sekarang)
-print('SSD Laptop setelah ditambah :', Laptop.SSD_sekarang + 256)
+laptop = My_Laptop()
+laptop.spesifikasi()
+laptop.kegunaan()
 
